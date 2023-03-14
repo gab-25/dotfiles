da88ff20fc2e89b3d46e1cd3ff5c600cef925537
Gabriele Sorci <71929275+gab-25@users.noreply.github.com>
2023-03-14 22:25:54 +0000

    init chatgpt cli

---
+++ .vimrc
@@ -0,0 +1,10 @@
+syntax enable
+set autoindent expandtab tabstop=2 shiftwidth=2
+set encoding=UTF-8
+set background=dark
+set termguicolors
+set number relativenumber
+set noswapfile
+set nowrap
+set autoread
+set ignorecase
--- install-pkgs.sh
+++ install-pkgs.sh
@@ -1,18 +1,54 @@
 #!/bin/bash
 
-#install packages
-sudo pacman -S --noconfirm docker vim htop code dbeaver virtualbox gimp inkscape latte-dock libreoffice-still
+PATH=$(dirname $(realpath "$0"))
+PATH_TMP="$PATH/tmp"
 
-#docker config
+#install packages
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y htop net-tools fzf ripgrep vim postgresql-client python3-pip
+
+sudo snap install postman gimp inkscape
+
+python3 -m pip install pip --upgrade
+
+#install docker
+curl -fsSL https://get.docker.com -o $PATH_TMP/get-docker.sh
+sudo sh $PATH_TMP/get-docker.sh
+rm -r $PATH_TMP/get-docker.sh
 sudo usermod -aG docker $USER
-sudo systemctl enable docker
-sudo systemctl start docker
+
+#install nodejs
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
+sudo apt-get install -y nodejs
+
+#install vs-code
+wget -O $PATH_TMP/vscode.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
+sudo apt install -y $PATH_TMP/vscode.deb
+rm -r $PATH_TMP/vscode.deb 
+
+#install dbeaver
+wget -O $PATH_TMP/dbeaver.deb https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb
+sudo apt install -y $PATH_TMP/dbeaver.deb
+rm -r $PATH_TMP/dbeaver.deb
+
+#install virtualbox
+wget -O $PATH_TMP/virtualbox.deb https://download.virtualbox.org/virtualbox/7.0.6/virtualbox-7.0_7.0.6-155176~Ubuntu~jammy_amd64.deb
+sudo apt install -y $PATH_TMP/virtualbox.deb
+rm -r $PATH_TMP/virtualbox.deb
 
 #install google-chrome
-echo Y | yay -S --nocleanmenu --nodiffmenu google-chrome
-
-#install postman
-echo Y | yay -S --nocleanmenu --nodiffmenu postman-bin
+wget -O $PATH_TMP/google-chrome.deb https://www.google.com/intl/it_it/chrome/thank-you.html?statcb=0&installdataindex=empty&defaultbrowser=0#
+sudo apt install -y $PATH_TMP/google-chrome.deb
+rm -r $PATH_TMP/google-chrome.deb
 
 #install mongodb-compass
-echo Y | yay -S --nocleanmenu --nodiffmenu mongodb-compass
+wget -O $PATH_TMP/mongodb-compass.deb https://downloads.mongodb.com/compass/mongodb-compass_1.36.0_amd64.deb?_ga=2.181712164.2038150423.1678827658-1828552554.1678621412
+sudo apt install -y $PATH_TMP/mongodb-compass.deb
+rm -r $PATH_TMP/mongodb-compass.deb
+
+#copy .vimrc
+cp ./.vimrc $HOME/.vimrc
+
+#add bash config
+echo "source /usr/share/doc/fzf/examples/key-bindings.bash" >> $HOME/.bashrc
+echo "alias chatgptcli='python3 $PATH/chatgpt_cli/chatgpt_cli.py'" >> $HOME/.bashrc
