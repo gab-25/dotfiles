10d202c5883252b56fb79c0074a8c9d31aa8a160
Gabriele Sorci <71929275+gab-25@users.noreply.github.com>
2023-06-19 19:23:41 +0200

    add lazygit

--- install-pkgs.sh
+++ install-pkgs.sh
@@ -14,7 +14,7 @@ python3 -m pip install pip --upgrade
 #install font Roboto
 wget -O $PATH_TMP/roboto.zip 'https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/RobotoMono.zip'
 unzip -q $PATH_TMP/roboto.zip -d $PATH_TMP/roboto
-rm -r $PATH_TMP/roboto.zip
+rm $PATH_TMP/roboto.zip
 mkdir -p $HOME/.local/share/fonts
 cp -r $PATH_TMP/roboto/*.ttf $HOME/.local/share/fonts
 rm -rf $PATH_TMP/roboto
@@ -23,7 +23,7 @@ fc-cache -fv
 #install docker
 curl -fsSL https://get.docker.com -o $PATH_TMP/get-docker.sh
 sudo sh $PATH_TMP/get-docker.sh
-rm -r $PATH_TMP/get-docker.sh
+rm $PATH_TMP/get-docker.sh
 sudo usermod -aG docker $USER
 
 #install nodejs
@@ -34,34 +34,41 @@ sudo apt-get install -y nodejs
 wget -O $PATH_TMP/neovim.tar.gz 'https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz'
 tar xzvf neovim.tar.gz
 mv neovim $HOME
-rm -r neovim.tar.gz
+rm neovim.tar.gz
 ln -s ~/neovim/bin/nvim .local/bin/nvim
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+
+#install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo $PATH_TMP/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf $PATH_TMP/lazygit.tar.gz lazygit
+install lazygit $HOME.local/bin
+rm $PATH_TMP/lazygit.tar.gz
 
 #install vs-code
 wget -O $PATH_TMP/vscode.deb 'https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64'
 sudo apt install -y $PATH_TMP/vscode.deb
-rm -r $PATH_TMP/vscode.deb 
+rm $PATH_TMP/vscode.deb 
 
 #install dbeaver
 wget -O $PATH_TMP/dbeaver.deb https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb
 sudo apt install -y $PATH_TMP/dbeaver.deb
-rm -r $PATH_TMP/dbeaver.deb
+rm $PATH_TMP/dbeaver.deb
 
 #install virtualbox
 wget -O $PATH_TMP/virtualbox.deb https://download.virtualbox.org/virtualbox/7.0.6/virtualbox-7.0_7.0.6-155176~Ubuntu~jammy_amd64.deb
 sudo apt install -y $PATH_TMP/virtualbox.deb
-rm -r $PATH_TMP/virtualbox.deb
+rm $PATH_TMP/virtualbox.deb
 
 #install google-chrome
 wget -O $PATH_TMP/google-chrome.deb https://www.google.com/intl/it_it/chrome/thank-you.html?statcb=0&installdataindex=empty&defaultbrowser=0#
 sudo apt install -y $PATH_TMP/google-chrome.deb
-rm -r $PATH_TMP/google-chrome.deb
+rm $PATH_TMP/google-chrome.deb
 
 #install mongodb-compass
 wget -O $PATH_TMP/mongodb-compass.deb https://downloads.mongodb.com/compass/mongodb-compass_1.36.0_amd64.deb?_ga=2.181712164.2038150423.1678827658-1828552554.1678621412
 sudo apt install -y $PATH_TMP/mongodb-compass.deb
-rm -r $PATH_TMP/mongodb-compass.deb
+rm $PATH_TMP/mongodb-compass.deb
 
 #install github-cli
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
