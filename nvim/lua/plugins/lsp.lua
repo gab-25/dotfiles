61a02b01338a7b4c16d1f7db0b5680d85d32cb61
Gab25 <gabrielesorci.25@gmail.com>
2023-09-13 14:45:09 +0200

    add python format in null-ls

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -11,7 +11,14 @@ return {
       ensure_installed = {
         "angular-language-server",
         "prisma-language-server",
+        "black",
       },
     },
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      table.insert(opts.sources, require('null-ls').builtins.formatting.black)
+    end,
   },
 }
