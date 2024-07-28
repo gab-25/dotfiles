f42eb6ca76f4e1d51321d5d69cab98a0bda32c1b
gab25 <gabrielesorci.25@gmail.com>
2024-07-28 18:43:39 +0200

    add tmux and zsh

--- install.sh
+++ install.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-#copy nvim configurations
+sudo apt-get update
+sudo apt-get install -y curl zsh tmux vim fzf fd-find ripgrep nodejs npm cargo
+
+#install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+#install neovim
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
 cp -r ./nvim/. $HOME/.config/lvim
 
 #copy dotfiles
-cp ./bashrc $HOME/.bashrc
+cp ./zshrc $HOME/.zshrc
 cp ./vimrc $HOME/.vimrc
 cp ./gitconfig $HOME/.gitconfig
+cp ./tmux.conf $HOME/.tmux.conf
 
 #copy fonts
 cp -r ./fonts/* $HOME/.fonts/*
