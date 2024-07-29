07ffca74e8467da8075b0e3ed50dd5dbde1114ef
gab25 <gabrielesorci.25@gmail.com>
2024-07-29 22:49:45 +0200

    download nvim and lazygit from install script

--- install.sh
+++ install.sh
@@ -9,14 +9,18 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 #install neovim
+curl -o /tmp/nvim-linux64.tar.gz https://github.com/neovim/neovim/releases/download/v0.10.1/nvim-linux64.tar.gz
+tar xzvf /tmp/nvim-linux64.tar.gz -C $HOME/nvim && rm /tmp/nvim-linux64.tar.gz
 bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+culr -o /tmp/lazygit-linux64.tar.gz https://github.com/jesseduffield/lazygit/releases/download/v0.43.1/lazygit_0.43.1_Linux_x86_64.tar.gz
+tar xzvf /tmp/lazygit-linux64.tar.gz -C $HOME/.local/bin && rm /tmp/lazygit-linux64.tar.gz
 cp -r ./nvim/. $HOME/.config/lvim
 
 #copy dotfiles
-cp ./zshrc $HOME/.zshrc
-cp ./vimrc $HOME/.vimrc
-cp ./gitconfig $HOME/.gitconfig
-cp ./tmux.conf $HOME/.tmux.conf
+cp ./.zshrc $HOME/.zshrc
+cp ./.vimrc $HOME/.vimrc
+cp ./.gitconfig $HOME/.gitconfig
+cp ./.tmux.conf $HOME/.tmux.conf
 
 #copy fonts
 cp -r ./fonts/* $HOME/.fonts/*
