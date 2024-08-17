fd08b0c0bbf36d34cb6b23aa6bf2db4435423713
Gab25 <gabrielesorci.25@gmail.com>
2024-08-17 08:54:43 +0200

    add lazyvim

--- install.sh
+++ install.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
+#TODO: trovare un modo per installare sempre le ultime versioni
 
 sudo apt-get update
-sudo apt-get install -y curl zsh tmux vim fzf fd-find ripgrep nodejs npm cargo
-
-#install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sudo apt-get install -y curl tmux vim fd-find ripgrep nodejs npm
 
 #install neovim
 curl -o /tmp/nvim-linux64.tar.gz https://github.com/neovim/neovim/releases/download/v0.10.1/nvim-linux64.tar.gz
 tar xzvf /tmp/nvim-linux64.tar.gz -C $HOME/nvim && rm /tmp/nvim-linux64.tar.gz
-bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+ln -s $HOME/nvim/bin/nvim -t $HOME/.local/bin
+
+#install lazygit
 culr -o /tmp/lazygit-linux64.tar.gz https://github.com/jesseduffield/lazygit/releases/download/v0.43.1/lazygit_0.43.1_Linux_x86_64.tar.gz
 tar xzvf /tmp/lazygit-linux64.tar.gz -C $HOME/.local/bin && rm /tmp/lazygit-linux64.tar.gz
-cp -r ./nvim/. $HOME/.config/lvim
+
+#install anaconda
+bash <(curl -s https://repo.anaconda.com/archive/Anaconda3-2024.06-1-Linux-x86_64.sh)
 
 #copy dotfiles
-cp ./.zshrc $HOME/.zshrc
 cp ./.vimrc $HOME/.vimrc
 cp ./.gitconfig $HOME/.gitconfig
 cp ./.tmux.conf $HOME/.tmux.conf
+cp ./nvim $HOME/.config/
 
 #copy fonts
 cp -r ./fonts/* $HOME/.fonts/*
