dae8ee1c0005f37ba976e1fd45310270e3f0d261
Gab25 <gabrielesorci.25@gmail.com>
2024-08-22 19:43:24 +0200

    add zed settings

--- install.sh
+++ install.sh
@@ -4,15 +4,8 @@ sudo apt-get update
 sudo apt-get install -y curl vim fd-find ripgrep nodejs npm
 
 #install anaconda
-bash <(curl -s https://repo.anaconda.com/archive/Anaconda3-2024.06-1-Linux-x86_64.sh)
-
-#install pycharm
-curl -o /tmp/pycharm-community-2024.2.tar.gz https://download.jetbrains.com/python/pycharm-community-2024.2.tar.gz
-tar -zxf /tmp/pycharm-community-2024.2.tar.gz -C $HOME/pycharm-community
-rm /tmp/pycharm-community-2024.2.tar.gz
-
-#install zed
-curl -f https://zed.dev/install.sh | sh
+ANACONDA3_LATEST=$(python latest_anaconda3.py)
+bash <(curl -s https://repo.anaconda.com/archive/$ANACONDA3_LATEST)
 
 #copy dotfiles
 cp ./.bashrc $HOME/.bashrc
