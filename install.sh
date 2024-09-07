3a5081e0d39c006d2c5051eb3e18a7b41e82f71c
Gab25 <gabrielesorci.25@gmail.com>
2024-09-07 12:01:55 +0200

    add nvm

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-
 sudo apt-get update
 sudo apt-get install -y curl vim zsh fd-find ripgrep
 
-#install nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-
 #install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#install nvm
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
 #copy dotfiles
 cp ./.zshrc $HOME/.zshrc
