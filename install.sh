97b3d9a2edde2f33a96c44af3c2fe6a089440af6
Gab25 <gabrielesorci.25@gmail.com>
2024-10-12 09:13:31 +0200

    remove nvm from install.sh

--- install.sh
+++ install.sh
@@ -5,9 +5,6 @@ sudo apt-get install -y curl vim fd-find ripgrep python3 python3-pip nodejs npm
 #install anaconda
 python install_anaconda3.py
 
-#install nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-
 #copy dotfiles
 cp ./.vimrc $HOME/.vimrc
 cp ./.gitconfig $HOME/.gitconfig
