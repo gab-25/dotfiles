abdfe4b71883d076a63af4b8705fcec62233d590
Gab25 <gabrielesorci.25@gmail.com>
2025-05-29 14:30:23 +0200

    add font jetbrains mono

--- install.sh
+++ install.sh
@@ -4,4 +4,13 @@ sudo apt update && sudo apt upgrade -y
 
 sudo apt install -y tmux neovim fzf fd-find ripgrep lazygit gcc zsh
 
+# install tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install font
+mkdir -p ~/.local/share/fonts
+unzip JetBrainsMono.zip -d ~/.local/share/fonts/JetBrainsMono
+fc-cache -fv
